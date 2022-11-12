cd9a173f667f66171f0ad1be9433712ac95c85b6
C4ptainx <alejandrotrujano549@gmail.com>
2022-11-12 13:12:30 +0000

    nuevos archivos

---
+++ tipos_numericos.py
@@ -0,0 +1,4 @@
+# numeros enteros (int)
+a = -1 # a esde tipo int y su valor es -1
+b = a + 2  #b es de tipo int y el resultado de este es 1
+print(b)
---
+++ variables.py
@@ -0,0 +1,24 @@
+#variables 
+
+# en la variable suma guardaremos el resultado de 1+2
+suma = 1 + 2
+print(suma)
+#asignamos un valor a la variable "a" de un valor de 1 (tipo entero int)
+a = 1
+print(a)
+#asignamos a la varibale "a" el resultado de la operacion 3 * 4
+a = 3 * 4 
+print(a)
+# asignamos a la variable "a" la cadena de texto "Python" (tipo cadena de texto String)
+a = "Python"
+print(a)
+# asignar un valor a diferentes variables
+a=b=c = 1
+print(a)
+print(b)
+print(c)
+#asignar diferentes valor a diferentes variables
+a, b, c = 1,2,3
+print(a)
+print(b)
+print(c)
